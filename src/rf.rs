9729d24cc8d4b3295001099709457f695114570f
Mohammad <mim.seyedi@gmail.com>
2025-07-09 23:22:04 +0330

    The 'rf' module was documented.

--- src/rf.rs
+++ src/rf.rs
@@ -1,9 +1,14 @@
+//! This module is related to the structure of the files that can be searched.
+//! Any file that is `readable` can be searched and the output is displayed using matching `Cells`.
+
+
 use std::io::{ BufRead, };
 use crate::{ read_file, count_query, };
 use crate::cells::{ Cell, };
 use crate::style::{ ANSIStyle, };
 
 
+/// This structure includes any `readable` file that holds search-related information.
 pub struct ReadableFile {
     path: String,
     query: String,
@@ -12,14 +17,17 @@ pub struct ReadableFile {
 
 
 impl ReadableFile {
+    /// `ReadableFile` Constructor.
     pub fn new(path: String, query: String, ignore_case: bool) -> Self {
         Self { path, query, ignore_case, }
     }
 
+    /// `path` attr getter.
     pub fn path(&self) -> &str {
         &self.path
     }
 
+    /// This method displays only the lines that are matched by `Cells`.
     pub fn print_cells(&self) {
         println!("\n{}\n", self.get_head());
         if let Ok(buffer) = read_file(&self.path) {
@@ -45,10 +53,12 @@ impl ReadableFile {
         }
     }
 
+    /// This method only displays the `number` of matches in the file along with the `file path`.
     pub fn print_count(&self) {
         println!("{}", self.get_head());
     }
 
+    /// This method uses `cells` to display all lines in the file alongside the `matched lines`.
     pub fn print_page(&self) {
         println!("\n{}\n", self.get_head());
         if let Ok(buffer) = read_file(&self.path) {
@@ -68,6 +78,12 @@ impl ReadableFile {
         }
     }
 
+    /// This method returns a constant `head` consisting of the
+    /// `file name` and the `number` of matching occurrences.
+    ///
+    /// # Returns
+    ///
+    /// The output is a new `String`.
     fn get_head(&self) -> String {
         format!(
             "{}- Filename: {}{}'{}'{} {}[{}]{}",
@@ -82,6 +98,16 @@ impl ReadableFile {
         )
     }
 
+    /// The task of this method is to `count` the matches in the file.
+    /// This is done with the help of functions in the main module.
+    ///
+    /// # Arguments
+    ///
+    /// * `query` - The string to search for.
+    ///
+    /// # Returns
+    ///
+    /// The output is a `usize`, which is the number of `matches`.
     fn get_count(&self, query: &str) -> usize {
         let mut c: usize = 0;
         if let Ok(buffer) = read_file(&self.path) {
