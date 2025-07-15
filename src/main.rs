ce01cbd9117edf27c03ebe9562b9f25d46b29f35
Mohammad <mim.seyedi@gmail.com>
2025-07-15 18:00:32 +0330

    The main file was documented.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+//! # lenz
+//!
+//! `lenz` is a command-line utility for searching and Browse files.
+//! It allows users to quickly find specific text patterns within files,
+//! with options for case-insensitive matching, counting occurrences,
+//! and different display modes.
+//!
+//! *GitHub repo: <https://github.com/mimseyedi/lenz>*
+
+
 use std::env;
 
 
